bb20a93821ff972897097ba7ab94336593522685
Marina Privalova <privalovama@yandex.ru>
2025-02-05 16:21:05 +0300

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php include("./incl/head.php") ?>
+
+<body class="page">
+  <main class="main">
+    <div class="container">
+      <section class="offer section">
+        <div class="offer-wrap">
+          <a class="logo-link hover-link" href="https://brightrich.ru/" aria-label="перейти на сайт компании">
+            <img src="./imgs/logo.svg" alt="логотип компании" loading="lazy">
+            <span>BRIGHT RICH | CORFAC INTERNATIONAL</span>
+          </a>
+          <h1>Real Estate Digest</h1>
+          <h2>Москва. Январь</h2>
+        </div>
+      </section>
+    </div>
+
+  </main>
+</body>
+
+</html>
