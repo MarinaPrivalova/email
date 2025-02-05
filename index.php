b08d253702c1ce58f853e231131b6693e306865e
Marina Privalova <privalovama@yandex.ru>
2025-02-05 16:32:11 +0300

    blocks

--- index.php
+++ index.php
@@ -15,8 +15,39 @@
           <h2>Москва. Январь</h2>
         </div>
       </section>
+      <section class="greeting section">
+        <div class="greeting-wrap">
+          <span>Добрый день, Александр!</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dignissim metus a dui pellentesque, eu condimentum enim dictum. Pellentesque ornare, metus quis gravida condimentum, orci odio sodales sapien, ut iaculis lectus massa non tellus. Nullam ultrices auctor justo quis accumsan. Duis mollis eleifend tellus, quis sodales augue elementum et. Aenean at consectetur eros.</p>
+          <div class="greeting-info-wrap">
+            <img src="./imgs/man.webp" alt="мужчина" loading="lazy">
+            <div class="greeting-info">
+              <p>Алексей Слепов</p>
+              <p>Director, W&I</p>
+              <a class="hover-link" href="tel:89992144020" aria-label="позвонить по номеру">+7 (999) 214-40-20</a>
+              <a class="hover-link" href="mailto:a.s@brightrich.moscow" aria-label="написать на почту">a.s@brightrich.moscow</a>
+            </div>
+          </div>
+          <div class="greeting-info-btns">
+            <a href="https://t.me/" aria-label="написать в телеграм" target="_blank"></a>
+            <a href="https://wa.me/79992144020" aria-label="написать в ватсап" target="_blank"></a>
+          </div>
+        </div>
+      </section>
+      <section class="cards section">
+        <h2>Заголовок</h2>
+        <div class="card card_red">
+          <p><span>237,6</span> тыс. кв. м</p>
+          <p>-5% от 2023 года <span>↓</span></p>
+          <p>объем вакантных площадей</p>
+        </div>
+        <div class="card card_green">
+          <p><span>405,5</span> руб./ кв. м</p>
+          <p>+5% от 2023 года <span>↑</span></p>
+          <p>средневзвешенная арендная ставка</p>
+        </div>
+      </section>
     </div>
-
   </main>
 </body>
 
